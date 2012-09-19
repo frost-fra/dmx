4ba2b71b3dfb6a794373bb21388b0ed8986a227b
Markus Baier <Markus_Baier@web.de>
2012-09-19 16:25:33 +0200

    added the dmx_test_tools folder

---
+++ dmx_test_tools/dmx_usb_test/src/dmx_usb_test.c
@@ -0,0 +1,46 @@
+/*
+ * $Id: dmx_usb_test.c 40 2004-09-11 11:16:39Z erwin $ 
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	unsigned char buffer[513];
+	int fd;
+	int res;
+	int i;
+
+	fd = open("/dev/dmx0",O_WRONLY);
+	if (fd < 0) {
+		perror("open");
+		exit(-1);
+	}
+
+	for (i = 0; i < 513;i++) {
+		buffer[i] = i;	
+	}
+
+	buffer[0] = 0x0A;
+	buffer[1] = 0x7F;
+	buffer[2] = 0xC8;
+	buffer[512] = 0x55;
+	
+	while(1) {
+		res = write(fd, buffer, 513);
+
+		if (res < 0){
+			perror("write");
+			exit(-1);
+		}
+	}
+
+	close(fd);
+
+	return 0;
+}
