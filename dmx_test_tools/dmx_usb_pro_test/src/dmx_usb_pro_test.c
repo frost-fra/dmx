502b5a48a4889084f6491475c64c19d1ede75603
Markus Baier <Markus_Baier@web.de>
2012-09-24 16:46:33 +0200

    file update

--- dmx_test_tools/dmx_usb_pro_test/src/dmx_usb_pro_test.c
+++ dmx_test_tools/dmx_usb_pro_test/src/dmx_usb_pro_test.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char* argv[])
 {
-	unsigned char buffer[513];
+	unsigned char buffer[31];
 	int fd;
 	int res;
 	int i;
@@ -27,6 +27,21 @@ int main(int argc, char* argv[])
 		exit(-1);
 	}
 
+	for (i = 5; i < 29;i++) {
+                buffer[i] = 0x78;
+        }
+
+	buffer[0] = 0x7E; //Startcode einer neuen Nachricht
+	buffer[1] = 0x06; //Es folgt eine Writeanweisung auf den Bus
+	buffer[2] = 0x19 & 0xFF; //Anzahl aller zu schreibenden Daten
+	buffer[3] = 0x19 >> 8;  //
+	buffer[4] = 0x00; //DMX Startcode
+
+
+	buffer[29] = 0xE7; //Endcode dieser Nachricht
+
+	write(fd, buffer, 30);
+	
 	printf("\nDer serielle USB Port wird wieder geschlossen.\n");
 
 	close(fd);
