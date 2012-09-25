1e06111f9814bdbeff2cf5ab26cbfdb148724cbd
Markus Baier <Markus_Baier@web.de>
2012-09-25 13:50:02 +0200

    files update

---
+++ dmx_test_tools/dmx_usb_pro_test/src/conio.h
@@ -0,0 +1,55 @@
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/select.h>
+#include <string.h>
+
+/* reads from keypress, doesn't echo */
+int getch(void)
+{
+  struct termios oldattr, newattr;
+  int ch;
+
+  tcgetattr( STDIN_FILENO, &oldattr );
+  newattr = oldattr;
+  newattr.c_lflag &= ~( ICANON | ECHO );
+  tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+  ch = getchar();
+  tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+  return ch;
+}
+
+/* reads from keypress, echoes */
+int getche(void)
+{
+  struct termios oldattr, newattr;
+  int ch;
+
+  tcgetattr( STDIN_FILENO, &oldattr );
+  newattr = oldattr;
+  newattr.c_lflag &= ~( ICANON );
+  tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+  ch = getchar();
+  tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+  return ch;
+}
+
+
+int kbhit(void)
+{
+  struct termios term, oterm;
+  int fd = 0;
+  int c = 0;
+
+  tcgetattr(fd, &oterm);
+  memcpy(&term, &oterm, sizeof(term));
+  term.c_lflag = term.c_lflag & (!ICANON);
+  term.c_cc[VMIN] = 0;
+  term.c_cc[VTIME] = 1;
+  tcsetattr(fd, TCSANOW, &term);
+  c = getchar();
+  tcsetattr(fd, TCSANOW, &oterm);
+  if (c != -1)
+  ungetc(c, stdin);
+  return ((c != -1) ? 1 : 0);
+}
--- dmx_test_tools/dmx_usb_pro_test/src/dmx_usb_pro_test.c
+++ dmx_test_tools/dmx_usb_pro_test/src/dmx_usb_pro_test.c
@@ -5,16 +5,24 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include "conio.h"
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <termios.h>
 
 int main(int argc, char* argv[])
 {
-	unsigned char buffer[31];
+	unsigned char buffer[30];
 	int fd;
 	int res;
 	int i;
+	int pd;
+	struct termios term;
+	bool exit = false;
+	int test = 0;
+	char c;
 
 	printf("\nTestprogramm fuer Enttec DMXUSB PRO\n");
 	printf("USB to DMX Interface\n");
@@ -24,27 +32,82 @@ int main(int argc, char* argv[])
 	if (fd < 0) {
 		printf("\nDer Port konnte nicht geoeffnet werden.\n");
 		printf("Die Anwendung wird daher nun beendet\n");
-		exit(-1);
+	//	exit(-1);
 	}
 
-	for (i = 5; i < 29;i++) {
-                buffer[i] = 0x78;
+	//Initialisiere das buffer Array
+
+	for (i = 0; i < 29; i++)
+	{
+          buffer[i] = 0x00;
         }
 
-	buffer[0] = 0x7E; //Startcode einer neuen Nachricht
-	buffer[1] = 0x06; //Es folgt eine Writeanweisung auf den Bus
-	buffer[2] = 0x19 & 0xFF; //Anzahl aller zu schreibenden Daten
-	buffer[3] = 0x19 >> 8;  //
-	buffer[4] = 0x00; //DMX Startcode
+	buffer[0] = 0x7E;	 //Startcode einer neuen DMX Nachricht
+	buffer[1] = 0x06;	 //Es folgt eine Writeanweisung auf den Bus
+	buffer[2] = 0x19 & 0xFF; //LSB Anzahl Datenfelder
+	buffer[3] = 0x19 >> 8;   //MSB Anzahl Datenfleder
+	buffer[4] = 0x00;	 //DMX Startcode
+	buffer[29] = 0xE7;	 //Endcode dieser DMX Nachricht
 
+	printf("\nAbbruch der Schleife ueber Tastendruck\n");
+	printf("Abbruch erfolgt nach einem Durchlauf der Schleife\n");
 
-	buffer[29] = 0xE7; //Endcode dieser Nachricht
+	printf("Wert von ECHO: %#x\n", ECHO);
+	printf("Wert von ~ ECHO: %#x\n", ~ECHO);
 
-	write(fd, buffer, 30);
+	tcgetattr(pd, &term);
+	printf("Inhalt vor ECHO: %#x\n", term.c_lflag);
+
+	//Loechen des ECHO Flags im Terminal
+	term.c_lflag &= ~ECHO;
+	//printf("Inhalt nach Loeschen des ECHO Flags: %#x\n", term.c_lflag);
+	//term.c_lflag |= ECHO;
+	//printf("Inhalt nach Setzen des ECHO Flags: %#x\n", term.c_lflag);
+	tcsetattr(pd, TCSANOW, &term);
+
+	while(!kbhit())
+	{
+
+	  //Schalte LED Nr. 1 ein
+	  printf("\nTeste LED Nr.1\n");
+
+	  buffer[5] = 127;
+	  write(fd, buffer, 30);
 	
+	  //Warte 2 Sekunden
+	  sleep(2);
+
+
+	  //Schalte LED Nr.1 aus und Nr.2 ein
+	  printf("Teste LED Nr.2\n");
+
+          buffer[5] = 0;
+	  buffer[6] = 127;
+          write(fd, buffer, 30);
+
+	  //Warte 2 Sekunden
+          sleep(2);
+
+          //Schalte LED Nr.2 aus und Nr.3 ein
+          printf("Teste LED Nr.3\n");
+
+          buffer[6] = 0;
+          buffer[7] = 127;
+          write(fd, buffer, 30);
+
+	  //Warte 2 Sekunden
+	  sleep(2);
+	  buffer[7] = 0;
+
+	}
+
+	//Setzen des ECHO Flags im Terminal
+        term.c_lflag |= ECHO;
+        tcsetattr(pd, TCSANOW, &term);
+
 	printf("\nDer serielle USB Port wird wieder geschlossen.\n");
 
-	close(fd);
+	//close(fd);
 
 	return 0;
 }
